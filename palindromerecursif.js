71671bf05cbe99285952512295d9f07a54098647
NAGATI86 <nagati-mohmed@hotmail.com>
2019-10-21 10:51:55 +0200

    semaine2

--- palindromerecursif.js
+++ palindromerecursif.js
@@ -2,8 +2,10 @@ function palindrome(mot) {
 if (( mot.length == 0 ) || (mot.length == 1)){
 return true
 } else { 
-    // completer
-    return false
+    if(mot[0]!=mot[mot.lenght-1]){
+        return false
+    } else  {
+        return palindrome (mot.substring(1,mot.lenght-1))     
 }}
 function estpalindrome(chaine) {
   if(palindrome(chaine)){
